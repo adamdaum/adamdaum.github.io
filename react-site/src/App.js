d37c579eed1a1519c5534ba37ed1f86abf16798e
Adam Daum <daumadam@gmail.com>
2023-02-11 14:56:40 -0800

    added new create react app version of site

---
+++ react-site/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <main role="main" className="inner cover top-buffer">
+        <h1 className="cover-heading">Adam Daum</h1>
+        <p className="lead">I'm a software developer.  I like to make stuff that's fun, fast, looks cool, and is easy to use.  
+            I love to learn, and explore the latest tech.  This basic site is hosted on <a href="https://pages.github.com">GitHub Pages</a>, and made with <a href="https://html5boilerplate.com/">HTML5 Boilerplate</a>, and <a href="https://getbootstrap.com/">Bootstrap 4</a>.
+              
+          </p>
+       
+      </main>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> Adam Daum.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
